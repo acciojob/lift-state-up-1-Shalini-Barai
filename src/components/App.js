2a6d53fcb3f629ba75d6773344997e6698b35e45
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-03-24 14:56:02 +0530

    LS1

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,24 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
+  
         {/* Do not remove the main div */}
+        const [show,setShow]=useState(false);
+   function handleClick(){
+    setShow(true);
+   }
+   
+  return (
+    <div className='parent'>
+      <h1>Parent Component</h1>
+      <div className='child'>
+      <h2>Child Component</h2>
+      <button onClick={handleClick}>Show Modal</button>
+      {show && <div><h1>Modal Content</h1><h3>This is modal content</h3></div>}
+      </div>
     </div>
   )
+   
 }
 
 export default App
